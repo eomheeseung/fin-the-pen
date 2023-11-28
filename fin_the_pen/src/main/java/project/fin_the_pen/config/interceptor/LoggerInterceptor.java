d4bc89905ae5dffbca87809019dc9fcb8ad52598
eomheeseung <jeaimeshe@gamil.com>
2023-11-28 20:36:22 +0900

    v2) interceptor, webconfig 추가

---
+++ fin_the_pen/src/main/java/project/fin_the_pen/config/interceptor/LoggerInterceptor.java
@@ -0,0 +1,24 @@
+package project.fin_the_pen.config.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("request uri: {}", request.getRequestURI());
+        log.info("request ip: {}", request.getRemoteHost());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("response status: {}", response.getStatus());
+    }
+}
---
+++ fin_the_pen/src/main/java/project/fin_the_pen/config/webConfig/WebConfig.java
@@ -0,0 +1,27 @@
+package project.fin_the_pen.config.webConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.fin_the_pen.config.interceptor.LoggerInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}

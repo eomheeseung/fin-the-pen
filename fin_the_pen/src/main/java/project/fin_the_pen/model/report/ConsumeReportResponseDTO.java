531712b49cea6e9d7c9c0af97d668b2aefd206f3
eomheeseung <jeaimeshe@gamil.com>
2024-01-04 18:33:32 +0900

    v2) report response 수정

--- fin_the_pen/src/main/java/project/fin_the_pen/model/report/ConsumeReportResponseDTO.java
+++ fin_the_pen/src/main/java/project/fin_the_pen/model/report/ConsumeReportResponseDTO.java
@@ -7,11 +7,10 @@ public class ConsumeReportResponseDTO {
     private String category;
     private int amount;
     private String rate;
-    private StringBuffer buffer = new StringBuffer();
 
     public ConsumeReportResponseDTO(String category, int amount, String rate) {
         this.category = category;
         this.amount = amount;
-        this.rate = buffer.append(rate).append("%").toString();
+        this.rate = rate + "%";
     }
 }

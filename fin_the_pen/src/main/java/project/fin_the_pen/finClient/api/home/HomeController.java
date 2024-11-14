ec28bb5cf3312d30a92393d1cae72a62e5316117
eomheeseung <jeaimeshe@gamil.com>
2024-11-14 22:55:50 +0900

    11.09 api/user/info create, modify -> token 수정

--- fin_the_pen/src/main/java/project/fin_the_pen/finClient/api/home/HomeController.java
+++ fin_the_pen/src/main/java/project/fin_the_pen/finClient/api/home/HomeController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,12 +77,12 @@ public class HomeController {
         }
     }
 
-    @PostMapping("/api/user/info")
+    /*@PostMapping("/api/user/info")
     public ResponseEntity<String> userInfo(Authentication authentication) {
         Object principal = authentication.getPrincipal();
 
         return ResponseEntity.ok(jwtService.getSocialTypeFromToken(principal.toString()));
-    }
+    }*/
 
 
     /**
